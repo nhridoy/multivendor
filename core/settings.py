bc8131f80ee5b638a9cf655b51aa3aaef8a08c63
imzulkar <gmzulkar@gmail.com>
2024-04-02 15:09:09 +0600

    feat: custom pagination added
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -140,11 +140,11 @@ REST_FRAMEWORK = {
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ),
     "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.BrowsableAPIRenderer",
         "utils.extensions.custom_renderer.CustomJSONRenderer",
-        # "rest_framework.renderers.JSONRenderer",
-        # "rest_framework.renderers.BrowsableAPIRenderer",
     ),
 
+    "DEFAULT_PAGINATION_CLASS": "utils.extensions.custom_pagination.CustomPagination",
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
