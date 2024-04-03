4a32b6825d64e382ec448264a3633bfed8d000b4
imzulkar <gmzulkar@gmail.com>
2024-04-03 15:18:12 +0600

    design: otp template design for email send
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

--- core/settings.py
+++ core/settings.py
@@ -86,7 +86,7 @@ ROOT_URLCONF = "core.urls"
 TEMPLATES = [
     {
         "BACKEND": "django.template.backends.django.DjangoTemplates",
-        "DIRS": [],
+        "DIRS": [APP_TEMPLATE_DIR],
         "APP_DIRS": True,
         "OPTIONS": {
             "context_processors": [
