f4f8212ec60ae19a7bab31aa34369620738a232f
imzulkar <gmzulkar@gmail.com>
2024-04-17 13:38:04 +0600

    update: admin model register
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

--- utils/modules/otp_verifications.py
+++ utils/modules/otp_verifications.py
@@ -1,6 +1,8 @@
 import pyotp
 from django.conf import settings
 
+from utils.modules import EmailSender
+
 
 class OTPVerification:
     secret_key: str
@@ -17,3 +19,13 @@ class OTPVerification:
 
     def verify_otp(self, otp_code):
         return self.totp.verify(otp_code.strip())
+
+    # def send_otp(self, send_to, name="User", company="Potential", procedure="Verification", valid_time="30 Min"):
+    #     self.otp_code = self.generate_otp()
+    #     email = EmailSender(subject="OTP Verification", send_to=send_to, context={"name": name, "company": company,
+    #                                                                               "otp_code": self.otp_code,
+    #                                                                               "procedure": procedure,
+    #                                                                               "valid_time": valid_time},
+    #                         template="email_templates/email_otp.html")
+    #     self.data = {"otp_code": self.otp_code}
+    #     return self.data
