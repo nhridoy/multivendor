6ecbfb2e28df34b0fa5d97e614a3ce1465ad85c7
imzulkar <gmzulkar@gmail.com>
2024-04-17 14:11:10 +0600

    feat: notice and notifications api created
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

--- notifications/models.py
+++ notifications/models.py
@@ -10,6 +10,7 @@ class Notifications(BaseModel):
     title = models.CharField(max_length=200, )
     content = models.TextField()
     is_read = models.BooleanField(default=False)
+    groups_notification = models.BooleanField(default=False)
 
     def __str__(self):
         return self.title
--- notifications/urls.py
+++ notifications/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
 from rest_framework.routers import DefaultRouter
 
+from notifications.views import NotificationsView
+
 router = DefaultRouter()
+
+router.register('notifications', NotificationsView, basename='notifications')
 
 urlpatterns = [
 
