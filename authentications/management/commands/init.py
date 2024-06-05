efb6181aedb04e7f25d2ebbfd6a2fc47e7de757b
imzulkar <gmzulkar@gmail.com>
2024-06-05 14:06:43 +0600

    refactor: ci/cd pipeline refactored for the backend projects
    Signed-off-by: imzulkar <gmzulkar@gmail.com>

---
+++ authentications/management/commands/init.py
@@ -0,0 +1,44 @@
+import os
+
+from django.contrib.auth import get_user_model
+from django.core.exceptions import ImproperlyConfigured
+from django.core.management.base import BaseCommand
+from decouple import config
+
+# from users.models import email_superuser
+
+SUPERUSER_USERNAME = config("SUPERUSER_USERNAME")
+SUPERUSER_EMAIL = config("SUPERUSER_EMAIL")
+SUPERUSER_PASSWORD = config("SUPERUSER_PASSWORD")
+
+if not SUPERUSER_PASSWORD:
+    raise ImproperlyConfigured("'SUPERUSER_PASSWORD' environment variable is unset")
+
+User = get_user_model()
+
+help_message = f"""
+Sets up the DB, creating:
+1) superuser with admin rights (Username: {SUPERUSER_USERNAME})
+"""
+
+
+class Command(BaseCommand):
+    """
+    init: Command to set up database for the application
+    """
+
+    help = help_message
+
+    def handle(self, *args, **kwargs):
+        if (
+                not User.objects.filter(username=SUPERUSER_USERNAME).exists()
+                or not User.objects.filter(email=SUPERUSER_EMAIL).exists()
+        ):
+            User.objects.create_superuser(
+                username=SUPERUSER_USERNAME,
+                email=SUPERUSER_EMAIL,
+                password=SUPERUSER_PASSWORD,
+                oauth_provider="email",
+            )
+            print("Super-User Created!")
+        print("Set-Up Complete!")
