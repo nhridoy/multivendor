5a141878e64a6e68dff45be347255b463f648b55
nhridoy <hridoyboss12@gmail.com>
2024-06-24 17:28:34 +0600

    boilerplate
    Signed-off-by: nhridoy <hridoyboss12@gmail.com>

--- authentications/user_manager.py
+++ authentications/user_manager.py
@@ -8,12 +8,16 @@ class UserManager(BaseUserManager):
     """
 
     @transaction.atomic
-    def create_user(self, username, email, password=None):
+    def create_user(self, username, email, password=None, oauth_provider=None):
         if not username:
             raise ValueError("Username should not be empty")
 
-        if not password:
+        if not oauth_provider:
+            raise ValueError("oauth_provider should not be empty")
+
+        if oauth_provider == "email" and not password:
             raise ValueError("Password should not be empty")
+
         if email:
             user = self.model(
                 username=username,
@@ -28,11 +32,12 @@ class UserManager(BaseUserManager):
         return user
 
     @transaction.atomic
-    def create_superuser(self, username, email, password=None):
+    def create_superuser(self, username, email, password=None, oauth_provider="email"):
+        if not password:
+            raise ValueError("Password should not be empty")
+
         user = self.create_user(
-            username=username,
-            email=email,
-            password=password,
+            username=username, email=email, password=password, oauth_provider=oauth_provider
         )
         user.is_superuser = True
         user.is_staff = True
