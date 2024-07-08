26029d4f88232f8c38c8ee61f5e0f73974e39a8b
Nahidujjaman Hridoy <hridoyboss12@gmail.com>
2024-07-08 16:27:06 +0600

    feature: cookie auth
    Signed-off-by: Nahidujjaman Hridoy <hridoyboss12@gmail.com>

--- core/settings/drf_settings.py
+++ core/settings/drf_settings.py
@@ -13,6 +13,7 @@ REST_FRAMEWORK = {
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": (
         "rest_framework_simplejwt.authentication.JWTAuthentication",
+        "dj_rest_auth.jwt_auth.JWTCookieAuthentication",
         "rest_framework.authentication.SessionAuthentication",
         "rest_framework.authentication.BasicAuthentication",
     ),
